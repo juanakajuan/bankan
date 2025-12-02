a0deec32541ae78131e67e295d8b0ba052d0dc0f
Juan Mireles <jmireles@torrentialdata.com>
2025-12-02 13:37:58 -0500

    fix: initialize the boardStore to save data to local storage

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+import "./assets/theme.css";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
-import "./assets/theme.css";
+import { useBoardStore } from "./stores/boardStore";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+app.use(pinia);
+
+const boardStore = useBoardStore();
+boardStore.initializeBoard();
 
 app.mount("#app");
